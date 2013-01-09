fb6253cfd2751c384584512c46eb423078893a6e
Beata Sumera <bsumera@future-processing.com>
2013-01-09 11:54:21 +0100

    Model_Movie added, Controller_Searcher with its view supplemented with genres checkbox and titles search

--- application/classes/Controller/Searcher.php
+++ application/classes/Controller/Searcher.php
@@ -5,11 +5,20 @@ class Controller_Searcher extends Controller_Template_Website {
     
     public function action_index()
     {        
-        $genres = ORM::factory('Genre');
-        $genres_list = $genres->find_all();
+        $genres = ORM::factory('Genre')->find_all();
+        $movies = ORM::factory('Movie')->find_all();
+        
+        foreach ($movies as $movie)
+        {
+            $titles[] = $movie->title;
+        }
+        
+        $titles_typeahead = '["'.implode('","', $titles).'"]';
 
         $this->template->actual_page = 'search';
         $this->template->content ='searcher/index';
+        $this->template->genres = $genres;
+        $this->template->titles = $titles_typeahead;
     }
 }
 
--- application/views/searcher/index.php
+++ application/views/searcher/index.php
@@ -1,27 +1,15 @@
-<?php $genres = array('thriller', 'horror'); ?>
-<?php $directors = array('Tim Burton', 'Ingmar Bergman', 'Quentin Tarantino'); ?>
-<?php $films = '["movie1", "movie2", "movie3", "bleble"]'; ?>
 <form>
     <div class="controls">
         <h4>Gatunki</h4>
         <?php foreach ($genres as $genre): ?>
         <label class="checkbox">
-            <input type="checkbox" name="genre" value="<?php echo $genre ?>"> <?php echo $genre ?> <br>
+            <input type="checkbox" name="genre" value="<?php echo $genre->name ?>"> <?php echo $genre->name ?> <br>
         </label>
         <?php endforeach; ?>
     </div>
     
     <div>
         <h4>Tytul filmu</h4>
-	<input type="text" data-provide="typeahead" data-source='<?php echo $films ?>'>
-    </div>
-    
-    <div>
-        <h4>Rezyser</h4>
-        <select id="element">
-            <?php foreach ($directors as $director): ?>
-            <option value="<?php echo $director ?>"><?php echo $director ?></option>
-            <?php endforeach; ?>
-        </select>
+	<input type="text" data-provide="typeahead" data-source='<?php echo $titles ?>'>
     </div>
 </form>
