8df980c51dff7ff000bd67066fb21a694187e7bb
Piotr Bartela <pbartela2@future-processing.com>
2013-01-10 10:51:15 +0100

    admin Cinema

--- application/classes/Controller/AdM/Cinema.php
+++ application/classes/Controller/AdM/Cinema.php
@@ -2,34 +2,44 @@
 
 class Controller_AdM_Cinema extends Controller_Admin {
 
-        public $template = 'index';
-        public $content ='adm_cinema/index';
-        
-        public function action_index() {               
-        $this->template->actual_page ='administrate';
+    public $template = 'index';
+    public $content = 'adm_cinema/index';
+
+    public function action_index() {
+        $this->template->actual_page = 'administrate';
         $seances = ORM::factory('seance')
-             ->find_all();
-        $this->template->seances=$seances; 
-        }
+                ->find_all();
+        $this->template->seances = $seances;
+    }
+
+    public function action_add_show() {
         
-        public function action_add_show()
-        {
-           
-        }
         
-        public function action_edit_show()
-        {
+        $this->_save(ORM::factory('seance'));
+    }
+
+    public function action_edit_show() {
         
+        $this->_save(ORM::factory('seance'));
+    }
+
+    public function action_delete_show() {
+        $shows = ORM::factory('seances', (int) $this->request->param('id', 0))
+                ->delete();
+        HTTP::redirect('adm');
+    }
+
+    private function _save($movies) {
+        $post = $this->request->post();
+        if (isset($post['username'])) {
+
+            try {
+                
+            } catch (ORM_Validation_Exception $e) {
+                $t = $e->errors('validation');;
+                $this->template->errors = $t;
+            }
         }
-        
-        public function action_delete_show()
-        {
-            $shows = ORM::factory('seances',(int)$this->request->param('id',0))
-                    ->delete();
-            HTTP::redirect('adm');
-        }
-        private function save()
-        {
-          
-        }
+    }
+
 }
