5b4213dd30e5527ce4f5acedd79b9c822d4d1d8a
Przemyslaw Kalita <pkalita@future-processing.com>
2013-01-10 09:32:06 +0100

    many-to-many relationships

--- application/classes/Controller/Seances.php
+++ application/classes/Controller/Seances.php
@@ -3,6 +3,7 @@
     {
         public $template='index';        
         public $content ='seances/index';
+        
         public function action_index()
         {
             
@@ -11,6 +12,19 @@
             {
                 $date = date('Y-m-d');
             }            
+            
+            $seances = ORM::factory('seance')->where('date', '=', $date)->find_all();
+            
+            $movies = ORM::factory('movie', 2);
+
+            $genres = $movies->genres->find_all();
+            
+            foreach ($genres as $genre)
+            {
+                echo $genre->name;
+            }
+            
+            //var_dump($genres);
             $days = Helper_DayCalculator::CalculateDays();
             $this->template->actual_page = 'home';
             $this->template->days = $days;
--- application/classes/Model/seances.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Model_Seances extends ORM
-{
-    
-}
-
-?>
